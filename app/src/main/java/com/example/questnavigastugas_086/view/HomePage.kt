931efb627452ccb85b8d65825bb5d0befed02f21
LindaPermata <permatalinda531@gmail.com>
2025-11-07 13:25:15 +0700

    Membuat column dengan text selamat datang

--- app/src/main/java/com/example/questnavigastugas_086/view/HomePage.kt
+++ app/src/main/java/com/example/questnavigastugas_086/view/HomePage.kt
@@ -1,7 +1,21 @@
 package com.example.questnavigastugas_086.view
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
@@ -13,3 +27,21 @@ fun HalamanAwal(navController: NavController) {
     val nimColor = Color.Black
     val namaFontSize = 23.sp
     val nimFontSize = 18.sp
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(backgroundColor)
+            .padding(32.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceAround
+    ){
+        Spacer(modifier = Modifier.height(30.dp))
+        Text(
+            text = "Selamat Datang",
+            fontSize = 42.sp,
+            fontWeight = FontWeight.Bold,
+            fontFamily = FontFamily.Monospace,
+            color = textColor,
+            style = TextStyle(lineHeight = 56.sp)
+        )
