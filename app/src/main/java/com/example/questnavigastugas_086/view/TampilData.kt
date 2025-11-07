4d956b75fd5ea41a5b38d44f78f616b331038915
LindaPermata <permatalinda531@gmail.com>
2025-11-07 13:36:19 +0700

    menambahkan composable ListDaftar dengan pengaturan warna

--- app/src/main/java/com/example/questnavigastugas_086/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigastugas_086/view/TampilData.kt
@@ -1,2 +1,15 @@
 package com.example.questnavigastugas_086.view
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun ListDaftar(navController: NavController) {
+    val headerPurple = Color(0xFFDC26D3)
+    val backgroundColor = Color(0xFFDDD3E3)
+    val cardBackgroundColor = Color.White
+
+    val buttonColor = Color(0xFFDC26D3)
+    val textColor = Color(0xFF8E8E93)
+    val buttonTextColor = Color.White
+    val labelPurple = Color(0xFF900CBB)
