7760d483d24934f6227502201def58a4f19d71a0
LindaPermata <permatalinda531@gmail.com>
2025-11-07 13:27:19 +0700

    Menambahkan code untuk impor gambarnya

--- app/src/main/java/com/example/questnavigastugas_086/view/HomePage.kt
+++ app/src/main/java/com/example/questnavigastugas_086/view/HomePage.kt
@@ -1,5 +1,6 @@
 package com.example.questnavigastugas_086.view
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -7,16 +8,19 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.questnavigastugas_086.R
 
 @Composable
 fun HalamanAwal(navController: NavController) {
@@ -45,3 +49,12 @@ fun HalamanAwal(navController: NavController) {
             color = textColor,
             style = TextStyle(lineHeight = 56.sp)
         )
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.fotoku),
+                contentDescription = "Logo Aplikasi",
+                modifier = Modifier.size(280.dp)
+            )
