de6989b5f7a079d50a564677ebf926e6f7742c41
LindaPermata <permatalinda531@gmail.com>
2025-11-07 13:23:50 +0700

    Menambahkan composable function

--- app/src/main/java/com/example/questnavigastugas_086/view/HomePage.kt
+++ app/src/main/java/com/example/questnavigastugas_086/view/HomePage.kt
@@ -1,2 +1,15 @@
 package com.example.questnavigastugas_086.view
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun HalamanAwal(navController: NavController) {
+    val backgroundColor = Color(0xFFF3E5F5)
+    val textColor = Color(0xFFDC26D3)
+    val buttonColor = Color(0xFFDC26D3)
+    val namaColor = Color(0xFFDC26D3)
+    val nimColor = Color.Black
+    val namaFontSize = 23.sp
+    val nimFontSize = 18.sp
